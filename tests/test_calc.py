4f2e7018895c4d9a7aad66935ebe36a30a07664e
RebeccaLundberg1 <rebecca93lundberg@gmail.com>
2026-04-13 22:13:57 +0200

    afgsd

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,99 +4,97 @@ calc = Calculator()
 result = []
 
 result.append([
-    "Add", 
-    "1, 1", 
-    2, 
-    calc.add(1,1), 
-    "Pass" if calc.add(1,1) == 2 else "Fail"
+    "Add",
+    "1, 1",
+    2,
+    calc.add(1, 1),
+    "Pass" if calc.add(1, 1) == 2 else "Fail"
 ])
 
 result.append([
-    "Subtract", 
-    "5, 1", 
-    4, 
-    calc.add(1,1), 
-    "Pass" if calc.subtract(5,1) == 4 else "Fail"
+    "Subtract",
+    "5, 1",
+    4,
+    calc.subtract(5, 1),
+    "Pass" if calc.subtract(5, 1) == 4 else "Fail"
 ])
 
 result.append([
-    "Multiply", 
-    "2, 4", 
+    "Multiply",
+    "2, 4",
     8,
-    calc.multiply(2,4),
-    "Pass" if calc.multiply(2,4) == 8 else "Fail"
+    calc.multiply(2, 4),
+    "Pass" if calc.multiply(2, 4) == 8 else "Fail"
 ])
 
 result.append([
-    "Divide", 
-    "6, 2", 
+    "Divide",
+    "6, 2",
     3,
-    calc.divide(6,2), 
-    "Pass" if calc.divide(6,2) == 3 else "Fail"
+    calc.divide(6, 2),
+    "Pass" if calc.divide(6, 2) == 3 else "Fail"
 ])
 
 result.append([
-    "Power", 
-    "2, 2", 
+    "Power",
+    "2, 2",
     4,
-    calc.power(2,2), 
-    "Pass" if calc.power(2,2) == 4 else "Fail"
+    calc.power(2, 2),
+    "Pass" if calc.power(2, 2) == 4 else "Fail"
 ])
 
 result.append([
-    "Sqrt", 
-    "16", 
+    "Sqrt",
+    "16",
     4,
-    calc.sqrt(16), 
+    calc.sqrt(16),
     "Pass" if calc.sqrt(16) == 4 else "Fail"
 ])
 
 result.append([
-    "Modulo", 
-    "10, 6", 
+    "Modulo",
+    "10, 6",
     4,
-    calc.modulo(10,6), 
-    "Pass" if calc.modulo(10,6) == 4 else "Fail"
+    calc.modulo(10, 6),
+    "Pass" if calc.modulo(10, 6) == 4 else "Fail"
 ])
 
-
-print(f"{"Metod":<12} | {"Input":<10} | {"Förväntat":<12} | {"Faktiskt":<12} | {"Pass/Fail"}")
-print("-"*80)
+print(f"{'Metod':<12} | {'Input':<10} | {'Förväntat':<12} | {'Faktiskt':<12} | {'Pass/Fail'}")
+print("-" * 80)
 for row in result:
     print(f"{row[0]:<12} | {row[1]:<10} | {row[2]:<12} | {row[3]:<12} | {row[4]}")
 
-
-if (calc.add(1,1) == 2):
+if calc.add(1, 1) == 2:
     print("Add = Korrekt")
-else: 
+else:
     print("Add = Wrong")
 
-if (calc.subtract(5,1) == 4):
+if calc.subtract(5, 1) == 4:
     print("Sub = Korrekt")
-else: 
+else:
     print("Sub = Wrong")
-    
-if (calc.multiply(2,4) == 8):
+
+if calc.multiply(2, 4) == 8:
     print("Mul = Korrekt")
-else: 
+else:
     print("Mul = Wrong")
-    
-if (calc.divide(6,3) == 2):
+
+if calc.divide(6, 3) == 2:
     print("Div = Korrekt")
-else: 
+else:
     print("Div = Wrong")
-    
-if (calc.power(2,2) == 4):
+
+if calc.power(2, 2) == 4:
     print("Power = Korrekt")
-else: 
+else:
     print("Power = Wrong")
-    
-if (calc.sqrt(16) == 4):
+
+if calc.sqrt(16) == 4:
     print("Sqrt = Korrekt")
-else: 
+else:
     print("Sqrt = Wrong")
-    
-if (calc.modulo(10, 6) == 4):
+
+if calc.modulo(10, 6) == 4:
     print("Mod = Korrekt")
-else: 
+else:
     print("Mod = Wrong")
